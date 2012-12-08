f8e0d476c045612028e95c50d6b06c8d76360f65
Christine McClure <camcclure@gmail.com>
2012-12-08 13:30:13 -0600

    added missing theme files

---
+++ wp/wp-content/themes/modernize-child/footer.php
@@ -0,0 +1,32 @@
+		<div class="footer-wrapper">
+                    <div id="footer1">
+                        <p>
+                                <strong>Cary's Lounge</strong>: 2251 West Devon Ave., Chicago, IL 60659  (773) 743-5737
+                        </p>
+                        <p>
+                        <strong>Hours</strong>: Monday - Friday 12pm-2am; Saturday: 12pm-3am; Sunday: 11am-2am
+                        </p>
+                    </div>
+                    <div id="footer2">
+                        <a target="_blank" href="https://www.facebook.com/pages/Carys-Lounge/149325854309"><img src="http://caryslounge.com/wp/wp-content/themes/modernize/images/icon/light/social/facebook.png" id="fb" title="Like us on Facebook"></a>
+                        <a href="/wp/contact"><img id="contact" src="http://caryslounge.com/wp/wp-content/themes/modernize/images/icon/light/social/email.png" title="Contact us"></a>
+                    </div>
+                    <div id="footer3">
+                        <p>&copy; 2012, Cary's Lounge</p>
+                        <p>
+                        Site by: <a href="http://www.caryslounge.com">Christine McClure</a>
+                        </p>     
+                    </div>
+                    
+		</div><!-- footer-wrapper -->
+	</div> <!-- container -->
+</div> <!-- body-wrapper -->
+	
+<?php wp_footer(); ?>
+
+<script type="text/javascript"> 	
+	<?php include ( TEMPLATEPATH."/javascript/cufon-replace.php" ); ?>
+</script>
+
+</body>
+</html>

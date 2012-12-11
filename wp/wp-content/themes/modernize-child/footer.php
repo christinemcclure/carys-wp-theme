e6fb62cde5857e6c6a93e9c448e3cf84bd2ca4df
Christine McClure <camcclure@gmail.com>
2012-12-10 20:45:08 -0600

    corrected link to my name

--- wp/wp-content/themes/modernize-child/footer.php
+++ wp/wp-content/themes/modernize-child/footer.php
@@ -1,22 +1,22 @@
 		<div class="footer-wrapper">
-                    <div id="footer1">
-                        <p>
-                                <strong>Cary's Lounge</strong>: 2251 West Devon Ave., Chicago, IL 60659  (773) 743-5737
-                        </p>
-                        <p>
-                        <strong>Hours</strong>: Monday - Friday 12pm-2am; Saturday: 12pm-3am; Sunday: 11am-2am
-                        </p>
-                    </div>
-                    <div id="footer2">
-                        <a target="_blank" href="https://www.facebook.com/pages/Carys-Lounge/149325854309"><img src="http://caryslounge.com/wp/wp-content/themes/modernize/images/icon/light/social/facebook.png" id="fb" title="Like us on Facebook"></a>
-                        <a href="/wp/contact"><img id="contact" src="http://caryslounge.com/wp/wp-content/themes/modernize/images/icon/light/social/email.png" title="Contact us"></a>
-                    </div>
-                    <div id="footer3">
-                        <p>&copy; 2012, Cary's Lounge</p>
-                        <p>
-                        Site by: <a href="http://www.caryslounge.com">Christine McClure</a>
-                        </p>     
-                    </div>
+      <div id="footer1">
+        <p>
+           <strong>Cary's Lounge</strong>: 2251 West Devon Ave., Chicago, IL 60659  (773) 743-5737
+        </p>
+        <p>
+        <strong>Hours</strong>: Monday - Friday 12pm-2am; Saturday: 12pm-3am; Sunday: 11am-2am
+        </p>
+      </div>
+      <div id="footer2">
+        <a target="_blank" href="https://www.facebook.com/pages/Carys-Lounge/149325854309"><img src="http://caryslounge.com/wp/wp-content/themes/modernize/images/icon/light/social/facebook.png" id="fb" title="Like us on Facebook"></a>
+        <a href="/wp/contact"><img id="contact" src="http://caryslounge.com/wp/wp-content/themes/modernize/images/icon/light/social/email.png" title="Contact us"></a>
+      </div>
+      <div id="footer3">
+        <p>&copy; 2012, Cary's Lounge</p>
+        <p>
+        Site by: <a href="http://www.christinemcclure.com">Christine McClure</a>
+        </p>
+      </div>
                     
 		</div><!-- footer-wrapper -->
 	</div> <!-- container -->
